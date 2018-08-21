a8e24cc58e2aba8622b368c20743007058d7703f
carrjona <carrjona@ecs.vuw.ac.nz>
2018-08-21 21:25:21 +1200

    Renamed dataHandler to ultimateDataHandler

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var config = require('./config')
 
-let dataLoader = require('./handlers/dataHandler')
+let dataLoader = require('./handlers/ultimateDataHandler')
 dataLoader.loadData()
 
 app.use('/', express.static(__dirname+'/public'));
