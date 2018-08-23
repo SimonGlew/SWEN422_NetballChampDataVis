1718ac882534b15ff9721460582236bdd168f5b0
glewsimo <glewsimo@ecs.vuw.ac.nz>
2018-08-23 23:23:15 +1200

    finished venue graph, just needs prettyifying so it doesnt look like its drawn by a 2 year old

--- server/public/venueComparison.js
+++ server/public/venueComparison.js
@@ -18,16 +18,20 @@ VenueComparison.getData = function (team, startDate, endDate, finals) {
 }
 
 VenueComparison.drawGraph = function () {
+	$('#venue-comparison-container').empty();
+
 	var margin = {
 		top: 50,
 		right: 10,
 		bottom: 60,
 		left: 60
 	},
-		width = $('.venue-comparison-chart').width() - margin.left - margin.right,
-		height = 500 - margin.top - margin.bottom;
+		width = $('#venue-comparison-container').width() - margin.left - margin.right,
+		height = 700 - margin.top - margin.bottom;
 
-	var svg = d3.select('.venue-comparison-chart').append("svg")
+	var axisHeight = height - 100;
+
+	var svg = d3.select('#venue-comparison-container').append("svg")
 		.attr("width", width + margin.left + margin.right)
 		.attr("height", height + margin.bottom + margin.top);
 
@@ -35,17 +39,24 @@ VenueComparison.drawGraph = function () {
 		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 	var y = d3.scaleLinear()
-		.range([height, 0])
+		.range([axisHeight, 0])
 		.domain([0, 100])
 
-	var x = d3.scaleOrdinal()
+	var x = d3.scaleBand()
 		.range([0, width])
+		.padding(0.1)
 		.domain(VenueComparison.venueList)
 
-	var xAxis = g.append("g")
-		.attr("class", "axis axis--x")
-		.attr("transform", "translate(0," + height + ")")
-		.call(d3.axisBottom(x).ticks(VenueComparison.venueList.length))
+	g.append("g")
+		//.attr("class", "axis axis--x")
+		.attr("transform", "translate(0," + axisHeight + ")")
+		.call(d3.axisBottom(x))
+		.selectAll("text")
+		.attr("y", 20)
+		.attr("x", -10)
+		.attr("dy", ".35em")
+		.attr("transform", "rotate(50)")
+		.style("text-anchor", "start");
 
 	var yAxis = g.append("g")
 		.attr("class", "axis axis--y")
@@ -65,9 +76,27 @@ VenueComparison.drawGraph = function () {
 		.attr("x", function (d) {
 			return x(d.venue);
 		})
-		.attr("width", 20)
-		.attr('height', function(d){
-			return height - y(d.win_rate); 
+		.attr("width", x.bandwidth())
+		.attr('height', function (d) {
+			return axisHeight - y(d.win_rate);
+		})
+		.on("mouseover", function (d) {
+			let x = d3.mouse(this)[0], y = d3.mouse(this)[1]
+
+			let split = d.toolTipVenue.split(',')
+
+			let html = '<div>' +
+				'<span class="block"><b>Venue: </b>' + split[0] + '</span >' +
+				'<span class="block"><b>At: </b>' + split[1] + '</span >' +
+				'<span class="block"><b>Games Played: </b>' + d.played + '</span>' +
+				'<span class="block"><b>Games Won: </b>' + d.won + '</span>' +
+				'<span class="block"><b>Winrate: </b>' + d.win_rate.toFixed(0) + '%</span>' +
+				'</div>'
+
+			showTooltip(x, y, html)
+		})
+		.on("mouseout", function (d) {
+			hideTooltip()
 		})
 
 }
